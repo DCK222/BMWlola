69cfe9f429b702292a156f56f76b050f8d4ea752
Alejandro García Dorronsoro <62794988+DCK222@users.noreply.github.com>
2023-10-20 10:05:40 +0200

    first commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Porfolio_Alejandro'; // Asegúrate de que esta propiedad esté definida
+
+  // Otro código de tu componente si lo tienes
+}
